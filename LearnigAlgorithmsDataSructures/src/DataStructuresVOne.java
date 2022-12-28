9e3d96498978bcc87fae2d0d6b7623722f205d40
E-Mabilo <62311541+E-Mabilo@users.noreply.github.com>
2022-12-28 12:53:34 +0200

    Update DataStructuresVOne.java
    Added the missing lines of code.

--- LearnigAlgorithmsDataSructures/src/DataStructuresVOne.java
+++ LearnigAlgorithmsDataSructures/src/DataStructuresVOne.java
@@ -26,7 +26,78 @@ public class DataStructuresVOne {
 //	}
 	
 	public static void main(String[] args) {
+		// dynamic arrays. Adding and removing items is possible 
+		// create a new array object 
+	
+		/*int voidIt = 2;
+		voidIt ++; 
+		int save = 5;
+		String names = "esther";
 		
+		Point pointA =new Point(10, 20); //x:10 y:20
+		
+		System.out.println(pointA);
+		System.out.println(names);
+		*/
+//		int[] numbers = new int[3];
+//		numbers[0] = 2;
+//		numbers[1] = 4;
+//		numbers[2] = 6;
+//		
+//		String[] names = {"esther", "prince", "thandi", "mami"};
+//		
+//		for (String name: names) {
+//			System.out.println((name));
+//		}
+//		
+//		//Forward display using for loop 
+//		for (int i = 0; i < names.length; i++ ) {
+//			System.out.println((names[i]));
+//		}
+//		
+//		
+//		//Reverse display using for loop 
+//		for (int i = names.length - 1; i > 0; i -- ) {
+//			System.out.println((names[i]));
+//		}
+////	
+//		System.out.println(Arrays.toString(names));
+//		int x, y, z;
+//		x =-14;
+//		z=-3; 
+//		y=5;
+//		int r =x%z;
+//		System.out.println(r);
+//		if(x>5 ){
+//	System.out.println("true");
+//	
+//		}	else
+//		{System.out.println("false");}
+		
+		
+		// Objects/referencing a class name to create a new reference object called alex from the static class Person "NAME"
+//		Person alex = new Person();
+//		
+//		Person alexer = alex ;
+//		
+//		System.out.println("alexer's name is:  " + alexer.name);
+//		
+//	}
+	
+	// Objects/referencing a class name to create a new reference object called alex from the static class Person "NAME"
+//	static class Person{
+//		String name = "me";
+//		void me(String name){
+//			this.name = name;
+//		}
+		int[] numbers = new int[3];
+		numbers[0] = 2;
+		numbers[1] = 4;
+		numbers[2] = 6;
+//		System.out.println(Arrays.toString(numbers));
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(i);
+		}
 	}
 	
 	
